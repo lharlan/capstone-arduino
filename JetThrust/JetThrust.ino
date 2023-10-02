e40c982bdf20913b50c12eef5518f91aea489f1a
Logan <75159044+lharlan@users.noreply.github.com>
2023-10-01 23:35:39 -0600

    Add JetThrust ino file

---
+++ JetThrust/JetThrust.ino
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string.h>
+#include <Wire.h> 				// Library for I2C communication
+#include <LiquidCrystal_I2C.h> 	// Library for LCD
+#include <HX711.h> 				// load cell library
+
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 20, 4);
+
+// HX711 wiring
+  // Thrust scale
+const int LOADCELL_TDT_PIN = 6;
+const int LOADCELL_TSCK_PIN = 7;
+
+  // Reverse thrust scale
+const int LOADCELL_RTDT_PIN = 4;
+const int LOADCELL_RTSCK_PIN = 3;
+
+HX711 scaleThrust;
+HX711 scaleRThrust;
+
+int thrustVal;
+int rthrustVal;
+
+void setup() {
+  
+  Serial.begin(57600);
+  // Initialize the display
+  lcd.init();
+  lcd.backlight();
+
+  // Initialize the Load Cells
+  scaleThrust.begin(LOADCELL_TDT_PIN, LOADCELL_TSCK_PIN);
+  scaleRThrust.begin(LOADCELL_RTDT_PIN, LOADCELL_RTSCK_PIN);
+
+  // Set the calibration values and reset the scale to zero
+  scaleThrust.set_scale(2);
+  scaleRThrust.set_scale(2)
+  scaleThrust.tare();
+
+}
+
+void loop() {
+  
+  thrustVal = scaleThrust.read();
+  rthrustVal = scaleRThrust.read();
+
+  lcd.setCursor(0, 0);
+  lcd.print("Back Thrust");
+
+  lcd.setCursor(3, 1);
+  lcd.print(thrustVal);
+
+  lcd.setCursor(0, 2);
+  lcd.print("Reverse Thrust");
+
+  lcd.setCursor(3, 4);
+  lcd.print(rthrustVal);
+
+  delay(1000);
+
+}
