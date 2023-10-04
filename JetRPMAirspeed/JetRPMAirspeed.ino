062b53e2caa8396e5752320a6f0dbd691ae8f656
Logan <75159044+lharlan@users.noreply.github.com>
2023-10-04 15:50:20 -0600

    Finished code, still needs testing

--- JetRPMAirspeed/JetRPMAirspeed.ino
+++ JetRPMAirspeed/JetRPMAirspeed.ino
@@ -1,8 +1,11 @@
 #include <Wire.h> 				      // Library for I2C communication
 #include <LiquidCrystal_I2C.h> 	// Library for LCD
 #include <SPI.h>
+#include <stdio.h>
+#include <string.h>
 
 LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 20, 4);
+String kpa = " kPa";
 
 // Tachometer Setup
 unsigned long rpmtime;
@@ -30,17 +33,24 @@ ISR(TIMER1_OVF_vect) {
 void loop() {
   
   // Read Airspeed value
-  int sensorValue = analogRead(A0);
+  int kPaValue = analogRead(A0);
+  String skPaValue = String(kPaValue);
+  skPaValue = skPaValue + kpa;
 
   lcd.setCursor(0, 0);
   lcd.print("Pressure");
 
   lcd.setCursor(3, 1);
-  lcd.print(sensorValue);
+  lcd.print(skPaValue);
 
   rpmfloat = 120 / (rpmtime/ 31250.00);
   rpm = round(rpmfloat);
 
+  lcd.setCursor(0, 2);
+  lcd.print("RPM");
+
+  lcd.setCursor(3, 4);
+  lcd.print(rpm);
 
 
   delay(1000);
