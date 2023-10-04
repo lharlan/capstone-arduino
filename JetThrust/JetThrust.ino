c9589c969417d550e83e6648f1299429931ab658
Logan <75159044+lharlan@users.noreply.github.com>
2023-10-04 15:50:57 -0600

    Updated lcd.print statements to include units

--- JetThrust/JetThrust.ino
+++ JetThrust/JetThrust.ino
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-#include <Wire.h> 				// Library for I2C communication
+#include <Wire.h> 				      // Library for I2C communication
 #include <LiquidCrystal_I2C.h> 	// Library for LCD
-#include <HX711.h> 				// load cell library
+#include <HX711.h> 				      // load cell library
 
 LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 20, 4);
+
+String kg = " kg";
 
 // HX711 wiring
   // Thrust scale
@@ -34,7 +36,7 @@ void setup() {
 
   // Set the calibration values and reset the scale to zero
   scaleThrust.set_scale(2);
-  scaleRThrust.set_scale(2)
+  scaleRThrust.set_scale(2);
   scaleThrust.tare();
 
 }
@@ -44,17 +46,23 @@ void loop() {
   thrustVal = scaleThrust.read();
   rthrustVal = scaleRThrust.read();
 
+  String sthrustVal = String(thrustVal);
+  String srthrustVal = String(rthrustVal);
+
+  sthrustVal = sthrustVal + kg;
+  srthrustVal = srthrustVal + kg;
+
   lcd.setCursor(0, 0);
   lcd.print("Back Thrust");
 
   lcd.setCursor(3, 1);
-  lcd.print(thrustVal);
+  lcd.print(sthrustVal);
 
   lcd.setCursor(0, 2);
   lcd.print("Reverse Thrust");
 
   lcd.setCursor(3, 4);
-  lcd.print(rthrustVal);
+  lcd.print(srthrustVal);
 
   delay(1000);
 
